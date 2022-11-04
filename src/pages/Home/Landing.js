682c08d87e56eb7280916bf8d9c49d37d9eef5d3
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-11-04 15:17:38 +0600

    hero section has been done with responsive issue

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -3,14 +3,17 @@ import BucketGirl from '../../assets/images/bucketgirl.png'
 
 const Landing = () => {
     return (
-        <div className="hero min-h-screen bg-base-200 mt-16">
+        <div className="hero h-screen lg:h-[60vh] bg-accent mt-16">
             <div className="hero-content flex-col lg:flex-row">                
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className='text-xl'>Best Quality</p>
+                    <h1 className="text-5xl font-bold">Professional Cleaning Service</h1>
+                    <p className="py-6 max-w-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
-                <img src={BucketGirl} className="max-w-sm rounded-lg shadow-2xl" />
+                <div className='h-[60vh] shrink-0'>
+                <img src={BucketGirl} className="h-full" />
+                </div>
             </div>
         </div>
     );
