26b4ff46b5b4c6f8e2ef46c1e997db48ac8193c0
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-11-06 02:47:47 +0600

    private route on a diffrent page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 import { useEffect } from "react";
+import PrivateRoute from "./authentication/PrivateRoute";
+import Services from "./pages/Services";
+import About from "./pages/About";
 
 
 function App() {
@@ -19,6 +22,10 @@ function App() {
               <Route key={index} path={path} element={<Component />} />
             ))
           }
+          <Route element={<PrivateRoute />}>
+            <Route path="/services" element={<Services />}/>
+            <Route path="/about" element={<About/>}/>
+          </Route>
         </Routes>
       </Navbar>
     </div>
