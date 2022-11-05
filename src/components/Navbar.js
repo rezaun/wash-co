960748a6a7c840d89fd64df1d5603142af13c1b6
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-11-06 02:11:56 +0600

    login page design has been done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import { useEffect } from 'react'
-import { themeChange } from 'theme-change'
+import React, { useState } from 'react';
 import { NavLink, Link } from "react-router-dom";
 
 const Navbar = ({ children }) => {
-    useEffect(() => {
-        themeChange(false)
-        // 👆 false parameter is required for react project
-      }, [])
+    const [dark, setDark] = useState(false);
     return (
         <div>
-            <div className="drawer drawer-end">
+            <div className="drawer drawer-end" data-theme={dark ? "dark" : "light"}>
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
 
@@ -30,7 +25,7 @@ const Navbar = ({ children }) => {
                                 <li><NavLink to="/about" className='rounded-lg'>About</NavLink></li>
                                 <li><NavLink to="/services" className='rounded-lg'>Services</NavLink></li>
                                 <li><NavLink to="/contact" className='rounded-lg'>Contact</NavLink></li>
-                                <li><NavLink to="/login" className='rounded-lg'>Login</NavLink></li>        
+                                <li><NavLink to="/login" className='rounded-lg'>Login</NavLink></li>
                                 {/* <li><button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">theme</button></li>                         */}
                                 <li className="dropdown dropdown-hover dropdown-end">
                                     <label tabIndex={0} className="btn btn-primary btn-outline rounded-lg m-1">Book Now</label>
@@ -42,7 +37,7 @@ const Navbar = ({ children }) => {
                                 <label className="swap swap-rotate">
 
                                     {/* <!-- this hidden checkbox controls the state --> */}
-                                    <input type="checkbox" data-toggle-theme="dark,light"/>
+                                    <input type="checkbox" onClick={() => setDark(!dark)} />
 
                                     {/* <!-- sun icon --> */}
                                     <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
@@ -62,18 +57,20 @@ const Navbar = ({ children }) => {
                     <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
 
-                        <li><NavLink to="/home" className='rounded-lg'>Home</NavLink></li>
+                        <li><NavLink to="/" className='rounded-lg'>Home</NavLink></li>
                         <li><NavLink to="/about" className='rounded-lg'>About</NavLink></li>
                         <li><NavLink to="/services" className='rounded-lg'>Services</NavLink></li>
                         <li><NavLink to="/contact" className='rounded-lg'>Contact</NavLink></li>
                         <li><NavLink to="/login" className='rounded-lg'>Login</NavLink></li>
-                        <li className="dropdown dropdown-hover dropdown-end">
-                            <label tabIndex={0} className="btn btn-primary btn-outline rounded-lg m-1">Book Now</label>
-                            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li><a>Item 2</a></li>
-                            </ul>
-                        </li>
+                        <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                            <div className="collapse-title text-xl font-medium">
+                                Book Now
+                            </div>
+                            <div className="collapse-content">
+                                <li><NavLink to="/contact" className='rounded-lg'>Contact</NavLink></li>
+                                <li><NavLink to="/login" className='rounded-lg'>Login</NavLink></li>
+                            </div>
+                        </div>
 
                     </ul>
 
