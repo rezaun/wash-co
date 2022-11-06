9845c4aac193ac83fec3a36366d4094d56c3f441
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-11-07 03:02:05 +0600

    adding addservice and add admin page in the dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,20 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashboardSidebar from '../../components/DashboardSidebar';
+import AddAdmin from './AddAdmin';
+import AddService from './AddService';
 
 const Dashboard = () => {
     return (
-        <div className="drawer drawer-mobile mt-16 bg-accent">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-            {/* <!-- Sidebar content here --> */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-          </ul>
-        
-        </div>
-      </div>
+       <DashboardSidebar>
+        <Routes>
+            <Route path='/dashboard/add-admin' element={<AddAdmin/>}></Route>
+            <Route path='/dashboard/add-service' element={<AddService/>}></Route>
+        </Routes>
+        </DashboardSidebar>
     );
 };
 
